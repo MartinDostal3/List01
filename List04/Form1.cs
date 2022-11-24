3c65a6f34bc835389a7343acbb3420a435d8bb4d
dostalm.04 <dostalm.04@spst.eu>
2022-11-24 10:36:24 +0100

    list04

---
+++ List04/Form1.cs
@@ -0,0 +1,81 @@
+namespace List04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<int> list = new List<int>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Random rnd = new Random();
+
+            int n = int.Parse(textBox1.Text);
+
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(rnd.Next(-100, 101));
+            }
+
+          
+
+            foreach (int cis in list)
+            {
+                listBox1.Items.Add(cis);
+            }
+           
+
+            int indexMin = -1;
+            int indexMax = -1;
+            int min = 101;
+            int max = -101; 
+
+            for(int i =0; i < list.Count();i++)
+            {
+                int x = list[i];
+                if(x<min)
+                {
+                    if(x%2!=0 && x%3 == 0)
+                    {
+                        min = x;
+                        indexMin = i;
+                    }
+                }
+
+                if (x > max)
+                {
+                    if (x % 2 == 0)
+                    {
+                        max = x;
+                        indexMax = i;
+                    }
+                }
+                
+
+
+            }
+           list[indexMax] = min;
+            list[indexMin] = max;
+            listBox2.Items.Clear();
+            foreach (int cis in list)
+            {
+                listBox2.Items.Add(cis);
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
