a53004e33bb4f709ae2bf38f4e1f4a2b1dbba5cb
dostalm.04 <dostalm.04@spst.eu>
2022-11-28 13:00:18 +0100

    08 a 9 chybi ochrana

---
+++ List08/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        List<char> pismena = new List<char>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pismena.Clear();
+            //vyrobí list charů
+          
+            for(int i = 0; i < textBox1.Text.Length; i++)
+            {
+                char znak = textBox1.Text[i];
+                pismena.Add(znak);
+            }
+
+
+            //kontrolni zobrazeni
+            listBox1.Items.Clear();
+            // *** cyklem foreach ***
+            foreach (char znak in pismena)
+            {
+                listBox1.Items.Add(znak);
+            }
+            // konec kontrolního zobrazení
+
+
+
+
+            //zpristupni tlacitka
+            foreach (Control control in this.Controls)
+            {
+                if(control is Button)
+                {
+                    control.Enabled = true;
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(button2.Text);
+
+            MessageBox.Show((sender as Button).Text);
+            (sender as Button).Enabled = false;
+
+            for(int i = 0; i < pismena.Count;i++)
+            {
+                if (pismena[i] == char.Parse((sender as Button).Text))
+                {
+                    pismena.RemoveAt(i);
+                }
+            }
+            Vypis();
+        }
+
+        private void Vypis()
+        {
+            textBox1.Text = String.Empty;
+            foreach( char znak in pismena)
+            {
+                textBox1.Text += znak;
+            }
+        }
+    }
+}
---
+++ List09/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Bitmap> obrazky = new List<Bitmap>();
+        int index;
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(Bitmap obrazek in imageList1.Images)
+            {
+                obrazky.Add(obrazek);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ověřit že v listobrazku jeste neco je
+            // náhodný obrázek z list obrazku zobrazit v picturebox
+            Random rnd = new Random();
+            index = rnd.Next(0, imageList1.Images.Count);
+
+            if(obrazky[index] != null)
+            {
+                pictureBox1.Image = obrazky[index];
+            }
+           
+
+
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //smazat obrazek v pictureboxu (null)
+            //smazat obrázek v list obrázků
+            pictureBox1.Image = null;
+            obrazky.RemoveAt(index);
+        }
+
+    }
+}
